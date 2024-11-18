e0180bbe84d308c3bcc95910450cc42f835f2b2d
苏向夜 <fu050409@163.com>
2024-11-18 22:21:09 +0800

    fix: version check

--- scripts/version-check.mjs
+++ scripts/version-check.mjs
@@ -1,15 +1,12 @@
 import axios from "axios";
 
-const pkg = process.argv[2];
-const version = process.argv[3];
-
 axios
   .get(
-    `https://api.github.com/repos/swpu-acm/algohub/git/refs/tags/${pkg}-v${version}`
+    `https://api.github.com/repos/swpu-acm/algohub/git/refs/tags/`
   )
   .then((response) => {
-    const data = JSON.parse(response.data);
-    console.log(data[data.length - 1].ref.split("-")[1]);
+    const data = response.data;
+    console.log(data[data.length - 1].ref.split("-v")[1]);
   })
   .catch((error) => {
     console.error(error.data);
