63b4a062fea928f157e485cf3be641964c96f940
苏向夜 <fu050409@163.com>
2024-11-20 12:57:32 +0800

    feat(profile): updating profile information and account activation

--- src/scripts/api.ts
+++ src/scripts/api.ts
@@ -35,3 +35,61 @@ export const register = async (form: Register) => {
     } as ErrorResponse;
   }
 };
+
+interface UploadAvatar {
+  id: string;
+  token: string;
+  file: File;
+}
+
+interface UploadAvatarResponse {
+  uri: string;
+}
+
+export const uploadAvatar = async (form: UploadAvatar) => {
+  try {
+    const response = await axios.put("/account/content/upload", form, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data as Response<UploadAvatarResponse>;
+  } catch (error) {
+    return {
+      success: false,
+      message: AxiosError.from(error).message,
+    } as ErrorResponse;
+  }
+};
+
+interface Profile {
+  avatar?: string;
+  signature?: string;
+  links?: string[];
+  nickname?: string;
+  sex?: boolean;
+  birthday?: string;
+  name?: string;
+  student_id?: string;
+  school?: string;
+  college?: string;
+  major?: string;
+}
+
+interface ProfileForm {
+  id: string;
+  token: string;
+  profile: Profile;
+}
+
+export const updateProfile = async (form: ProfileForm) => {
+  try {
+    const response = await axios.post("/account/profile", form);
+    return response.data as Response<undefined>;
+  } catch (error) {
+    return {
+      success: false,
+      message: AxiosError.from(error).message,
+    };
+  }
+};
--- src/scripts/store.ts
+++ src/scripts/store.ts
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const prefersDarkMode = () => {
   return (
@@ -29,6 +29,57 @@ export const useThemeStore = defineStore(
       }
     };
     return { dark, toggle, init };
+  },
+  {
+    persist: true,
+  }
+);
+
+interface Account {
+  id?: string;
+  token?: string;
+
+  username: string;
+  email: string;
+  avatar?: string;
+  signature?: string;
+  links?: string[];
+
+  nickname?: string;
+  sex?: boolean;
+  birthday?: Date;
+
+  name?: string;
+  student_id?: string;
+  school?: string;
+  college?: string;
+  major?: string;
+
+  rating?: number;
+  role?: number;
+  active?: boolean;
+}
+
+export const useAccountStore = defineStore(
+  "account",
+  () => {
+    const account = ref<Account | null>(null);
+
+    const isLoggedIn = computed(
+      () => account.value !== null && account.value.token
+    );
+
+    const mergeProfile = (profile: Partial<Account>) => {
+      if (account.value) {
+        Object.assign(account.value, profile);
+      }
+    };
+
+    const logout = () => {
+      account.value = null;
+    };
+
+    return { account, isLoggedIn, mergeProfile, logout };
   },
   {
     persist: true,
