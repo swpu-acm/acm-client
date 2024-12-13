802cc86be8d766b315614b214d7822c967921645
落 <x.0602141017@gmail.com>
2024-12-13 19:35:47 +0800

    feat(org): inrt ui

--- src/views/org/create.vue
+++ src/views/org/create.vue
@@ -23,7 +23,7 @@ const start_time = ref<Date>();
 const end_time = ref<Date>();
 
 const inProgress = ref(false);
-const onCreateContest = async () => {
+const onCreateOrg = async () => {
     inProgress.value = true;
     const res = await api.createContest({
         auth: accountStore.auth!,
@@ -45,6 +45,8 @@ const onCreateContest = async () => {
 }
 </script>
 
+
+
 <template>
     <div class="flex-1 flex flex-col">
         <UniversalToolBar :path></UniversalToolBar>
@@ -52,28 +54,46 @@ const onCreateContest = async () => {
             <Panel class="mt-10 w-full h-full">
                 <div class="flex flex-col gap-8 w-full">
                     <div class="mt-10 text-center">
-                        <span class="text-gray-500 mb-4">Create a new contest</span>
-                        <h1 class="text-3xl font-bold">Create contest</h1>
+                        <span class="text-gray-500 mb-4">Tell us about your organization</span>
+                        <h1 class="text-3xl font-bold">Set up your organization</h1>
                     </div>
-                    <div class="flex flex-row gap-4">
-                        <div class="flex flex-col">
+                    <div class="flex flex-col">
+                        <!-- <div class="flex flex-col">
                             <label for="owner">Owner *</label>
                             <Select name="owner" disabled></Select>
                         </div>
                         <span class="flex flex-col justify-end">
                             <span class="text-bold mb-2">/</span>
-                        </span>
+                        </span> -->
                         <div class="flex flex-col">
-                            <label for="name">Name *</label>
+                            <label for="name" style="font-size: 20px;">Organization Name *</label>
+                            <InputText v-model="name" name="name"></InputText>
+                        </div>
+                        <div>
+                            <span class="text-gray-500 mb-4" style="font-size:13px">This will be the name of your
+                                account on AlgoHub.</span>
+                        </div>
+                        <div class="mt-6 flex flex-col">
+                            <label for="name" style="font-size: 20px;">Contact Email *</label>
                             <InputText v-model="name" name="name"></InputText>
                         </div>
                     </div>
-                    <MarkdownEditor v-model="description" placeholder="Description"></MarkdownEditor>
+                    <!-- <MarkdownEditor v-model="description" placeholder="Description"></MarkdownEditor>
                     <div class="flex flex-row gap-4">
                         <DatePicker v-model="start_time" placeholder="Start date" showTime></DatePicker>
                         <DatePicker v-model="end_time" placeholder="End date" showTime></DatePicker>
+                    </div> -->
+                    <div class="flex flex-col gap-1 w-full">
+                        <div class="flex items-center gap-2">
+                            <Checkbox inputId="terms" name="terms" binary />
+                            <label for="terms" class="text-sm">I have read and agree to the <a href="#"
+                                    class="underline">Affero
+                                    General Public License v3</a>.</label>
+                        </div>
+                        <!-- <Message v-if="$form.terms?.invalid" severity="error" size="small" variant="simple">{{
+                        $form.terms.error.message }}</Message> -->
                     </div>
-                    <Button @click="onCreateContest" :loading="inProgress" label="Save Changes"></Button>
+                    <Button @click="onCreateOrg" :loading="inProgress" label="Next"></Button>
                 </div>
             </Panel>
         </div>
